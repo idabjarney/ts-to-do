f6f26ecdc352fbafb86375be2de269141e4b0bfa
idaism <ida.bjarney@outlook.com>
2023-03-21 15:08:19 +0000

    logic for radio and bin btn finished

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import './root.scss'
 import './styles.scss'
 
+
+declare const friconix_update: () => void;
+
+
 const taskInputEL: HTMLInputElement | null = document.querySelector('.text-input-box');
 const submitBtn: HTMLButtonElement | null = document.querySelector('.submit-btn');
 let tasks: string[] = [];
@@ -12,21 +16,36 @@ function renderNewTask(e: Event): void {
   if (!taskInputEL?.value) return;
   const toDosUl: HTMLUListElement | null = document.querySelector('.to-dos-container')
   let inputValue: string  = taskInputEL?.value;
-  // const li: HTMLLIElement = document.createElement('li');
-  // const paragraph: HTMLParagraphElement = document.createElement('p');
-  // paragraph.innerText = inputValue;
-  // li.appendChild(paragraph);
-  // li.classList.add('to-do-el');
-  // toDosUl?.appendChild(li);
-  const newTodo: string = `
-  <li class="to-do-el">
-    <button class="to-do-selector"></button>
-    <p>${inputValue}</p>
-    <button class="bin-btn">hey
-    </button>
-  </li>
-  `;
-  toDosUl?.insertAdjacentHTML('beforeend', newTodo)
+  const li: HTMLLIElement = document.createElement('li');
+  const paragraph: HTMLParagraphElement = document.createElement('p');
+  const radioBtn: HTMLButtonElement = document.createElement('button');
+  const binBtn: HTMLButtonElement = document.createElement('button');
+  radioBtn.classList.add('to-do-selector');
+  binBtn.classList.add('bin-btn');
+  paragraph.innerText = inputValue
+  const icon: HTMLElement = document.createElement('i');
+  icon.classList.add('fi-xnsuxl-trash-solid');
+  binBtn.appendChild(icon);
+  li.appendChild(radioBtn);
+  li.appendChild(paragraph);
+  li.appendChild(binBtn);
+  li.classList.add('to-do-el');
+  toDosUl?.appendChild(li);
+  friconix_update(); //icon
+  tasks.push(inputValue);
+  console.log(tasks)
+
+  radioBtn.addEventListener('click', function(): void {
+    radioBtn.classList.toggle('radio-toggled');
+    li.classList.toggle('completed-task');
+  })
+
+  binBtn.addEventListener('click', function(): void {
+    li.remove();
+  })
+
+
+  taskInputEL.value = '';
 }
 
 // function createListItem(value: string): void {
