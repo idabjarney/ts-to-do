2f2e045a18274e3e869a9aa15f020301fe560c48
idaism <ida.bjarney@outlook.com>
2023-03-24 11:19:56 +0000

    finished to-do list with local storage

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ declare const friconix_update: () => void;
 
 const taskInputEL: HTMLInputElement | null = document.querySelector('.text-input-box');
 const submitBtn: HTMLButtonElement | null = document.querySelector('.submit-btn');
-const tasks: Tasks = {};
+let tasks: Tasks = {};
 
 
 enum TaskStatus { 
@@ -20,39 +20,22 @@ interface Tasks {
 }
 
 interface Task {
-  task: string,
+  taskValue: string,
   _id: number,
   status: TaskStatus
 }
 
-submitBtn?.addEventListener('click', renderNewTask);
+submitBtn?.addEventListener('click', createTask);
+window.addEventListener('DOMContentLoaded', getTasksFromStorage);
 
-function renderNewTask(e: Event): void {
+function createTask(e: Event): void {
   e.preventDefault();
   if (!taskInputEL?.value) return;
-  const toDosUl: HTMLUListElement | null = document.querySelector('.to-dos-container')
   let inputValue: string  = taskInputEL?.value;
-  const li: HTMLLIElement = document.createElement('li');
-  const paragraph: HTMLParagraphElement = document.createElement('p');
-  const radioBtn: HTMLButtonElement = document.createElement('button');
-  const binBtn: HTMLButtonElement = document.createElement('button');
-  radioBtn.classList.add('to-do-selector');
-  binBtn.classList.add('bin-btn');
-  paragraph.innerText = inputValue
-  const icon: HTMLElement = document.createElement('i');
-  icon.classList.add('fi-xnsuxl-trash-solid');
-  binBtn.appendChild(icon);
-  li.appendChild(radioBtn);
-  li.appendChild(paragraph);
-  li.appendChild(binBtn);
-  li.classList.add('to-do-el');
-  toDosUl?.appendChild(li);
-  friconix_update(); //icon
-
 
   const _id: number = new Date().getTime();
   const task: Task = {
-    task: inputValue,
+    taskValue: inputValue,
     _id: _id,
     status: TaskStatus.IN_PROGRESS
   }
@@ -60,25 +43,57 @@ function renderNewTask(e: Event): void {
   tasks[`${_id}`] = task;
   saveToStorage()
 
+  renderNewTask(task);
+  taskInputEL.value = '';
+}
+
+function renderNewTask(task: Task): void {
+  const toDosUl: HTMLUListElement | null = document.querySelector('.to-dos-container')
+  const li: HTMLLIElement = document.createElement('li');
+  const paragraph: HTMLParagraphElement = document.createElement('p');
+  const radioBtn: HTMLButtonElement = document.createElement('button');
+  const binBtn: HTMLButtonElement = document.createElement('button');
+  radioBtn.classList.add('to-do-selector');
+  binBtn.classList.add('bin-btn');
+  paragraph.innerText = task.taskValue;
+  const icon: HTMLElement = document.createElement('i');
+  icon.classList.add('fi-xnsuxl-trash-solid');
+  binBtn.appendChild(icon);
+  li.appendChild(radioBtn);
+  li.appendChild(paragraph);
+  li.appendChild(binBtn);
+  li.classList.add('to-do-el');
+  if(task.status === TaskStatus.COMPLETED) {
+    li.classList.add('completed-task');
+    radioBtn.classList.add('radio-toggled');
+  }
+  toDosUl?.appendChild(li);
+  friconix_update(); //icon
+
+
+
+
+
+
   radioBtn.addEventListener('click', function(): void {
     radioBtn.classList.toggle('radio-toggled');
     li.classList.toggle('completed-task');
-    if (tasks[`${_id}`].status === TaskStatus.IN_PROGRESS) {
-      tasks[`${_id}`].status = TaskStatus.COMPLETED
+    if (tasks[`${task._id}`].status === TaskStatus.IN_PROGRESS) {
+      tasks[`${task._id}`].status = TaskStatus.COMPLETED
+      saveToStorage();
     } else {
-      tasks[`${_id}`].status = TaskStatus.IN_PROGRESS
+      tasks[`${task._id}`].status = TaskStatus.IN_PROGRESS
+      saveToStorage();
     }
-    saveToStorage();
   })
 
   binBtn.addEventListener('click', function(): void {
     li.remove();
-    delete tasks[`${_id}`];
+    delete tasks[`${task._id}`];
     saveToStorage();
   })
 
 
-  taskInputEL.value = '';
 }
 
 function saveToStorage(): void {
@@ -89,9 +104,10 @@ function getTasksFromStorage(): void {
   const storedTasksString: string | null = localStorage.getItem('tasks');
   if (!storedTasksString) return;
   const storedTaskObj: Tasks = JSON.parse(storedTasksString);
+  tasks = storedTaskObj;
   const tasksValues: Task[] = Object.values(storedTaskObj);
   for (let task of tasksValues) {
-    
+    renderNewTask(task);
   }
 }
 
